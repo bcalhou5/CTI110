1a11d19d3d076936a2a88529823c37e1dad7b42f
bcalhou5 <calhounb2875@student.faytechcc.edu>
2017-06-08 01:23:47 -0400

    Add files via upload
    Distance Travelled

---
+++ M2HW1_DistanceTraveled_Calhoun.py
@@ -0,0 +1,9 @@
+# Distance Travelled
+# 06/05/17
+# CTI-110 M2HW1 - Distance Traveled
+# Benjamin Calhoun
+
+speed=70
+print("In 6 hours, the car will travel " + str(( speed * 6 ))+ " miles")
+print("In 10 hours, the car will travel " + str(( speed * 10 ))+ " miles")
+print("In 15 hours, the car will travel " + str(( speed * 15 ))+ " miles")
