43c57f21cb8cbcb0bd8c2a96b8d5449bd274b039
bcalhou5 <calhounb2875@student.faytechcc.edu>
2017-06-11 16:43:13 -0400

    Add files via upload
    Debugging Work Lab

---
+++ M3T2__Debugging_BenjaminCalhoun.py
@@ -0,0 +1,25 @@
+# Debugging
+# 06/11/17
+# CTI-110 M3T2 - Debugging
+# Benjamin Calhoun
+
+A_score=90
+B_score=80
+C_score=70
+D_score=60
+
+score=int(input('enter your test score:'))
+if score >=A_score:
+    print('Your grade is A.')
+else:
+    if score >=B_score:
+        print('Your grade is B.')
+    else:
+        if score >=C_score:
+            print('Your grade is C.')
+        else:
+            if score >=D_score:
+                print('Your grade is D.')
+            else:
+                print('Your grade is F.')
+                
