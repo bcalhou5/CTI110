96a610e2dc2760262e725a412ce327b8aabb4339
bcalhou5 <calhounb2875@student.faytechcc.edu>
2017-06-25 09:01:38 -0400

    Turtle Shapes

---
+++ M4LAB1_BenjaminCalhoun.py
@@ -0,0 +1,36 @@
+import turtle
+
+wn = turtle.Screen()
+
+
+wn.bgcolor("lightblue")
+wn.title("Shapes")
+
+kyle = turtle.Turtle()
+
+kyle.color("red")
+kyle.pensize(3)
+
+matt = turtle.Turtle()
+
+kyle.forward(80)
+kyle.left(120)
+kyle.forward(80)
+kyle.left(120)
+kyle.forward(80)
+kyle.left(120)
+
+kyle.right(180)
+kyle.forward(80)
+
+matt.forward(50)
+matt.left(90)
+matt.forward(50)
+matt.left(90)
+matt.forward(50)
+matt.left(90)
+matt.forward(50)
+matt.left(90)
+           
+
+wn.mainloop()
